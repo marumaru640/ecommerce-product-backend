7b93cc2ce38c7abf229889d92a6b42639e996762
KimYoungSoo <zerowater4704@gmail.com>
2025-09-19 18:32:27 +0900

    pom.xml test/runtime modification, loacl => local (profile name typo modification)

--- src/main/java/com/marumaru640/ecommerce_product_backend/member/MemberService.java
+++ src/main/java/com/marumaru640/ecommerce_product_backend/member/MemberService.java
@@ -1,7 +1,5 @@
 package com.marumaru640.ecommerce_product_backend.member;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.marumaru640.ecommerce_product_backend.mapper.MemberMapper;
@@ -9,13 +7,9 @@ import com.marumaru640.ecommerce_product_backend.member.dto.MemberCreateRequest;
 import com.marumaru640.ecommerce_product_backend.member.dto.MemberResponse;
 
 import jakarta.transaction.Transactional;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 public class MemberService {
-	
-	Logger logger = LoggerFactory.getLogger(MemberService.class);
 	
 	private final MemberRepository repo;
 	private final MemberMapper mapper;
@@ -33,9 +27,7 @@ public class MemberService {
 	
 	@Transactional
 	public MemberResponse create(MemberCreateRequest req) {
-		logger.debug("req email={}, name={}, pwd={}", req.email(), req.name(), req.password());
 		Member entity = mapper.toEntity(req);
-		logger.debug("mapped -> email={}, name={}, paws={}",entity);
 		Member saved = repo.save(entity);
 		return mapper.toResponse(saved);
 	}
