2074e38795839ec2db2db1a7550fe52c8ec1edd7
KimYoungSoo <zerowater4704@gmail.com>
2025-09-29 07:03:42 +0900

    login api implementation

---
+++ src/main/java/com/marumaru640/ecommerce_product_backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.marumaru640.ecommerce_product_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/api/auth/login").permitAll()
+					.anyRequest().authenticated()
+			);
+		return http.build();
+	}
+}
